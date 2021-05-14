7322989a7156cebbfc29f182fd7bbc3ed05484dc
Smeeksha <er.sharma2k14@gmail.com>
2021-05-14 22:36:56 +0530

    Calling Intent

---
+++ app/src/main/java/com/example/smeekshaapp/course.java
@@ -0,0 +1,14 @@
+package com.example.smeekshaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
